87294b894929289a6acf286ee9202d333b4cf430
Adam Faur <adam.faur@gmail.com>
2024-07-08 12:13:02 -0700

    🍞 Initial commit.

---
+++ Main.hx
@@ -0,0 +1,14 @@
+class Main extends hxd.App {
+
+  override private function init():Void {
+    super.init();
+    var myscene = new h2d.Scene();
+    setScene(myscene);
+  }
+
+  static function main() {
+    new Main();
+  }
+
+}
+
---
+++ install.sh
@@ -0,0 +1,4 @@
+haxelib git heaps https://github.com/HeapsIO/heaps.git
+haxelib git hlsdl https://github.com/HaxeFoundation/hashlink master libs/sdl
+haxelib install hashlink
+
