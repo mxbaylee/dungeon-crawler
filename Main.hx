9dfb85ff4d8c5221ee28e28895722ca7d149e90c
Adam Faur <adam.faur@gmail.com>
2024-07-09 07:05:56 -0700

    refactor

--- Main.hx
+++ Main.hx
@@ -5,6 +5,43 @@ enum abstract DirtPath (Int) to Int {
   var Bottom = 182;
 }
 
+/*
+  var tileMap = {
+    bottom: 153,
+    right: 154,
+    rightLeft: 155,
+    left: 155,
+  }
+  153-160
+  182-189
+*/
+
+function roguelikeTiles() {
+  var tileImage = hxd.Res.roguelikeDungeon_transparent.toTile();
+
+  var tw: Int = 16;
+  var th: Int = 16;
+
+  var margin: Int = 1;
+
+  return [
+    for (y in 0 ... Std.int((tileImage.height + margin) / (th + margin)))
+    for (x in 0 ... Std.int((tileImage.width + margin) / (tw + margin)))
+    tileImage.sub(x * (tw + margin), y * (th + margin), tw, th)
+  ];
+}
+
+function toSprite(tile, x = 0, y = 0) {
+  var sprite = new h2d.Bitmap(tile);
+
+  sprite.x = x;
+  sprite.y = y;
+  sprite.scaleX = 4;
+  sprite.scaleY = 4;
+
+  return sprite;
+}
+
 class Main extends hxd.App {
   override private function init():Void {
     super.init();
@@ -15,60 +52,12 @@ class Main extends hxd.App {
 
     myscene.scaleMode = h2d.Scene.ScaleMode.Stretch(320, 240);
 
-    var tileImage = hxd.Res.roguelikeDungeon_transparent.toTile();
+    var rlTiles = roguelikeTiles();
 
-    var tw: Int = 16;
-    var th: Int = 16;
-
-    var margin: Int = 1;
-
-    var tiles = [
-      for (y in 0 ... Std.int((tileImage.height + margin) / (th + margin)))
-      for (x in 0 ... Std.int((tileImage.width + margin) / (tw + margin)))
-      tileImage.sub(x * (tw + margin), y * (th + margin), tw, th)
-    ];
-
-/*
-    var tileMap = {
-      bottom: 153,
-      right: 154,
-      rightLeft: 155,
-      left: 155,
-    }
-    */
-    /*
-        153-160
-        182-189
-    */
-
-    var sprite = new h2d.Bitmap(tiles[DirtPath.Top]);
-
-    sprite.x = 0;
-    sprite.y = 0;
-    sprite.scaleX = 4;
-    sprite.scaleY = 4;
-    myscene.addChild(sprite);
-
-    var sprite = new h2d.Bitmap(tiles[160]);
-    sprite.x = 75;
-    sprite.y = 0;
-    sprite.scaleX = 4;
-    sprite.scaleY = 4;
-    myscene.addChild(sprite);
-
-    var sprite = new h2d.Bitmap(tiles[DirtPath.Bottom]);
-    sprite.x = 0;
-    sprite.y = 150;
-    sprite.scaleX = 4;
-    sprite.scaleY = 4;
-    myscene.addChild(sprite);
-
-    var sprite = new h2d.Bitmap(tiles[189]);
-    sprite.x = 75;
-    sprite.y = 150;
-    sprite.scaleX = 4;
-    sprite.scaleY = 4;
-    myscene.addChild(sprite);
+    myscene.addChild(toSprite(rlTiles[DirtPath.Top]));
+    myscene.addChild(toSprite(rlTiles[160], 75));
+    myscene.addChild(toSprite(rlTiles[DirtPath.Bottom], 0, 150));
+    myscene.addChild(toSprite(rlTiles[189], 75, 150));
   }
 
   static function main() {
